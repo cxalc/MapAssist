64ba9832d5d66ba2edb3d1971fbb1964f046e3a6
ItzRabbs <maxim.rabinovich1@gmail.com>
2021-12-16 09:57:57 -0500

    Don't change eth and socket non-white to super color in log

--- Helpers/Compositor.cs
+++ Helpers/Compositor.cs
@@ -618,13 +618,19 @@ namespace MapAssist.Helpers
                 if (isEth)
                 {
                     itemLabelExtra += "[Eth] ";
-                    fontColor = Items.ItemColors[ItemQuality.SUPERIOR];
+                    if (fontColor == Color.White)
+                    {
+                        fontColor = Items.ItemColors[ItemQuality.SUPERIOR];
+                    }
                 }
 
                 if (ItemLog[i].Stats.TryGetValue(Stat.STAT_ITEM_NUMSOCKETS, out var numSockets))
                 {
                     itemLabelExtra += "[" + numSockets + " S] ";
-                    fontColor = Items.ItemColors[ItemQuality.SUPERIOR];
+                    if (fontColor == Color.White)
+                    {
+                        fontColor = Items.ItemColors[ItemQuality.SUPERIOR];
+                    }
                 }
 
                 var brush = CreateSolidBrush(gfx, fontColor, 1);
